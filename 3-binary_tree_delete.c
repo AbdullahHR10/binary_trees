569772a72cb6a45527fc32c2688646609698f666
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-04-10 01:35:15 -0700

    Fixed if condition

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -8,9 +8,10 @@
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (tree == NULL)
-		exit(1);
-	binary_tree_delete(tree->right);
-	binary_tree_delete(tree->left);
-	free(tree);
+	if (tree != NULL)
+	{
+		binary_tree_delete(tree->right);
+		binary_tree_delete(tree->left);
+		free(tree);
+	}
 }
