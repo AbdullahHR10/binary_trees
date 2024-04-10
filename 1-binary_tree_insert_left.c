329dc42deb5bb69093e28ef52727b8cf3add5b91
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-04-09 22:43:56 -0700

    Fixed if condition

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,12 +8,18 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *newNode;
+
 	if (parent == NULL)
 		return (NULL);
+	newNode = binary_tree_node(parent, value);
+	if (newNode == NULL)
+		return (NULL);
 	if (parent->left != NULL)
-		parent->left = NULL;
-	binary_tree_t *newNode = binary_tree_node(parent->left, value);
-
+	{
+		newNode->left = parent->left;
+		newNode->left->parent = newNode;
+	}
 	parent->left = newNode;
 	return (newNode);
 }
