0d19528b3bbe256fbb77f58ca85803f28fec4bbb
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-04-10 01:33:18 -0700

    Wrote the function

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - Deletes an entire binary tree
+ * @tree: Tree to be deleted
+ * Return: Nothing
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		exit(1);
+	binary_tree_delete(tree->right);
+	binary_tree_delete(tree->left);
+	free(tree);
+}
