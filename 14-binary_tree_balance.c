2d437bb67e38664aac6710d5eb96eaa4a8483177
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-04-10 22:18:29 -0700

    Fixed the function

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -30,11 +30,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	size_t left_height, right_height, value;
 
 	if (tree == NULL)
 		return (0);
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-	return ((int)left_height - (int)right_height);
+	left_height = ((int)binary_tree_height(tree->left));
+	right_height = ((int)binary_tree_height(tree->right));
+	value  = left_height - right_height
+	return (value);
 }
