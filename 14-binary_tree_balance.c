1036c9d28a33d3891d2907e6a075754561967267
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-04-11 00:58:45 -0700

    Fixed a typo

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -14,8 +14,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	if (tree)
 	{
-		left_height = tree->left ? 1 + binary_tree_height2(tree->left) : 0;
-		right_height = tree->right ? 1 + binary_tree_height2(tree->right) : 0;
+		left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 	}
 	return (left_height > right_height ? left_height : right_height);
 }
