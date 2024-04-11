6ee24b95a5900e3ee6f0b626d82018ba4941537b
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-04-11 00:34:03 -0700

    Fixed

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -30,7 +30,7 @@ size_t binary_tree_height2(const binary_tree_t *tree)
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height, right_height;
+	int left_height = 0, right_height = 0;
 
 	if (tree == NULL)
 		return (0);
