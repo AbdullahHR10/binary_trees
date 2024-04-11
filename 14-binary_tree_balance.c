71b9ef2adf7427914bd83fc41be3e9ccd7960a9d
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-04-11 00:30:48 -0700

    Fixed syntax

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -34,7 +34,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	left_height = ((int)binary_tree_height(tree->left));
-	right_height = ((int)binary_tree_height(tree->right));
+	left_height = (int)binary_tree_height(tree->left);
+	right_height = (int)binary_tree_height(tree->right);
 	return (left_height - right_height);
 }
