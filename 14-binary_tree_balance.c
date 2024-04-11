c6fcb156e06ca47b61baa32e375a4a425f51ca01
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-04-11 00:31:51 -0700

    Changed height function name

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,7 +8,7 @@
  * Return: 0 if tree is NULL, otherwise the height of the tree
  */
 
-size_t binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_height2(const binary_tree_t *tree)
 {
 	size_t left_height = 0, right_height = 0;
 
@@ -34,7 +34,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	left_height = (int)binary_tree_height(tree->left);
-	right_height = (int)binary_tree_height(tree->right);
+	left_height = (int)binary_tree_height2(tree->left);
+	right_height = (int)binary_tree_height2(tree->right);
 	return (left_height - right_height);
 }
