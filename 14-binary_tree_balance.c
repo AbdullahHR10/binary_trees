a5acf05928c289e86953c4374c2b7dcde48cdcd7
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-04-11 00:29:11 -0700

    Changed return value

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -30,12 +30,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height, right_height, value;
+	int left_height, right_height;
 
 	if (tree == NULL)
 		return (0);
 	left_height = ((int)binary_tree_height(tree->left));
 	right_height = ((int)binary_tree_height(tree->right));
-	value = left_height - right_height;
-	return (value);
+	return (left_height - right_height);
 }
