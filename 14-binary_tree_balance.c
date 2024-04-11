8aaa7326a14f59f00c8a0d26a8728fb0d16f70fe
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-04-10 22:02:34 -0700

    Wrote the function

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - Measures the balance factor of a binary tree
+ * @tree: Tree to be measured
+ * Return: Balance factor of the node
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+	return ((int)(left_height - right_height));
+}
