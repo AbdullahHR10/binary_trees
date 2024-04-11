0bf96cfbcbde864f91701fee99f51e0ae4b7a185
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-04-11 01:09:29 -0700

    Starter code

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect - Checks if a binary tree is perfect
+ * @tree: Tree to be checked
+ * Return: 1 if tree is perfect, otherwise 0
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+}
